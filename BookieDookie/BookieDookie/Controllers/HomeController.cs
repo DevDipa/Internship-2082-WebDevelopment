8f87bde9335387d32a44be50dad6736be1d57741
Dipa <tamangdipa77@gmail.com>
2026-03-09 07:58:21 +0545

    feat(Home): add Bookmark card

--- BookieDookie/BookieDookie/Controllers/HomeController.cs
+++ BookieDookie/BookieDookie/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using BookieDookie.Data;
 using Microsoft.AspNetCore.Mvc;
 using BookieDookie.Models;
 using BookieDookie.Services.Interface;using BookieDookie.Services.Interface;
@@ -9,16 +10,42 @@ namespace BookieDookie.Controllers
     public class HomeController : Controller
     {
         private readonly IUserService _userService;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(IUserService userService)
+        public HomeController(IUserService userService, ApplicationDbContext context)
         {
             _userService = userService;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            ViewBag.ActiveUsers = _userService.GetActiveUsers();
-            ViewBag.InactiveUsers = _userService.GetInactiveUsers();
+            var user = _context.Users.FirstOrDefault();
+
+            var stats = _context.ReadingStats
+                .FirstOrDefault(s => s.UserId == user.Id);
+
+            int hour = DateTime.Now.Hour;
+            string greeting;
+
+            if (hour >= 5 && hour <= 7)
+                greeting = "Goodie morning! Ready to grow today?";
+            else if (hour >= 8 && hour <= 11)
+                greeting = "Hope your morning is unfolding gently.";
+            else if (hour >= 12 && hour <= 15)
+                greeting = "Ummhmm...a little afternoon-escape for a few pages, huh?";
+            else if (hour >= 16 && hour <= 19)
+                greeting = "A quiet evening for reflection.";
+            else if (hour >= 20 && hour <= 22)
+                greeting = "Want a gentle read before bed?";
+            else
+                greeting = "Still awake? No worries. BookieDookie's here to befriend you ^.^";
+
+            //ViewBag.UserName = user?.Name ?? "Reader";
+            ViewBag.GreetingLine = greeting;
+
+            ViewBag.BookmarkBook = stats?.BookmarkBook;
+            ViewBag.BookmarkPage = stats?.BookmarkPage;
 
             return View();
         }
